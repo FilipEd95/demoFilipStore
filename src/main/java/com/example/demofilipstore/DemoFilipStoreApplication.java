4c8317e34daeda97d61d60c7fb10e41c5c94f4d1
Filip Edfors <Filip_fottbolen@hotmail.com>
2024-11-10 11:26:38 +0100

    Initial commit

---
+++ src/main/java/com/example/demofilipstore/DemoFilipStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.demofilipstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFilipStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFilipStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demofilipstore/DemoFilipStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demofilipstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFilipStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
